aea47f4f46ce4318707923e3e22520d30929ef70
Andrei-Ve <andrei.velikainen@gmail.com>
2025-04-28 21:30:52 +0300

    modified the code and added some expl

--- main.c
+++ main.c
@@ -1,63 +1,55 @@
-#define F_CPU 16000000UL
+#define F_CPU 160000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
 
-// ==== UART SETUP ====
-
-// Initialize UART0
-void uart_init(unsigned int baud) {
-	unsigned int ubrr = (F_CPU / 16 / baud) - 1;
-	UBRR0H = (ubrr >> 8);
-	UBRR0L = ubrr;
-	UCSR0B = (1 << TXEN0);   // Enable transmitter
-	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);  // 8 data bits, 1 stop bit
-}
-
-// Send one byte
-void uart_send(char data) {
-	while (!(UCSR0A & (1 << UDRE0)));
+//sending a byte
+void USART0_Transmit(unsigned char data) {  //send byte
+	while (!(UCSR0A & (1 << UDRE0)));  //wait for empty buffer
 	UDR0 = data;
-}
+ }
 
-// Send a string
-void uart_send_string(const char* str) {
-	while (*str) {
-		uart_send(*str++);
+void USART0_TransmitStr(const char* str) {	//from string, send byte by byte to function above
+	while(*str) {
+		USART0_Transmit(*str++);
 	}
 }
 
-// Send integer as string
-void uart_send_int(uint16_t value) {
-	char buffer[10];
-	sprintf(buffer, "%u\r\n", value);
-	uart_send_string(buffer);
+void USART0_TransmitInt(uint16_t value) {
+	char buffer[10];   //create buffer
+	sprintf(buffer, "%u\r\n", value); //int into char[] 
+	USART0_TransmitStr(buffer);
 }
 
-// ==== ADC SETUP ====
-
-// Initialize ADC0 (PC0)
-void adc_init(void) {
-	ADMUX = (1 << REFS0); // AVcc as reference voltage, select ADC0 (PC0)
-	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1); // Enable ADC, prescaler = 64
-}
-
-// Read ADC value
-uint16_t adc_read(void) {
-	ADCSRA |= (1 << ADSC); // Start conversion
-	while (ADCSRA & (1 << ADSC)); // Wait until conversion complete
+uint16_t read_ADC(void) {
+	ADCSRA |= (1 << ADSC); //Begin conversion
+	while (ADCSRA & (1 << ADSC));  //waiting until its done
 	return ADC;
 }
 
-// ==== MAIN ====
-
 int main(void) {
-	uart_init(9600); // Initialize UART at 9600 baud
-	adc_init();      // Initialize ADC
+	//setting up ADC
+	ADMUX = 0;   //clear everything just in case   
+	ADMUX |= (1 << REFS0); //V_ref as AV_cc
+		//MUX is already at 000
+	ADCSRA = 0;  //clear everything just in case   
+	ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);  // enabling ADC and setting prescaler to 128
+														//   16MHz/128 = 125kHz
+	ADCSRB = 0;
 
-	while (1) {
-		uint16_t mic_value = adc_read();  // Read microphone analog value
-		uart_send_int(mic_value);          // Send value over UART
-		_delay_ms(100);                   // Small delay (adjust if needed)
+
+	//setting up UART0	
+		//Baud rate   //  UBRR0 = 16000000/(16*19200)-1 = 51
+	UBRR0 = 0;
+	UBRR0H = (51 >> 8);   //BRR Low and High
+	UBRR0L = 51;
+
+	UCSR0B |= (1 << TXEN0); //Enable TX
+	UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);// 8 data bits, 1 stop bit
+
+	while(1) {
+		uint16_t mic_adc = read_ADC(); //read value of mic
+		USART0_TransmitInt(mic_adc);
+		_delay_ms(1);//can add delay
 	}
 }
